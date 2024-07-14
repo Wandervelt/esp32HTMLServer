9cc8ee5bcfe09194aec608d45964b9265178a314
Wandervelt <87107858+Wandervelt@users.noreply.github.com>
2024-07-14 18:28:53 +0300

    Update code.ino
    Added deep sleep mood

--- code.ino
+++ code.ino
@@ -5,11 +5,16 @@
 #include <uri/UriBraces.h>
 
 #define WIFI_SSID "TP-Link_0633"
-#define WIFI_PASS "PASS_W"
+#define WIFI_PASS "PASS"
 
 WebServer server(80);
 
-const int adcPin = 1;  
+const int adcPin = 1;
+const int wakeupPin = 2; // added pin for external wake up
+const uint64_t SLEEP_DURATION = 30e6; // 30 seconds
+const unsigned long AWAKE_TIME = 30000;
+
+RTC_DATA_ATTR int bootCount = 0; // added boot count, uses RTC memory to store value
 
 float readVoltage() {
   int adcValue = analogRead(adcPin);
@@ -29,18 +34,28 @@ void sendHtml() {
       </style>
     </head>
     <body>
-      <h1>ESP32-S3 Web Server</h1>
-      <p>Hey buddy</p>
+      <h1>ESP32 Deep Sleep Demo</h1>
+      <!-- Added boot count display -->
+      <p>Boot Count: <span id='bootCount'></span></p>
       <p>Voltage reading: <span id='reading'></span>V</p>
-      <button onclick="getReading() ">Update Reading</button>
+      <!-- Added sleep timer display -->
+      <p>Time until sleep: <span id='sleepTimer'></span> seconds</p>
       <script>
         function getReading() {
-            fetch('/reading')
-            .then(response => response.text())
-            .then(reading => document.getElementById('reading').textContent = reading);
+          fetch('/reading')
+            .then(response => response.json())
+            .then(data => {
+              document.getElementById('bootCount').textContent = data.bootCount;
+              document.getElementById('reading').textContent = data.voltage;
+              });
+        }
+        function updateSleepTimer() {
+          let timeLetf = 30 - (Math.floor((Date.now()/1000) % 30));
+          document.getElementById('sleepTimer').textContent = timeLetf;
         }
         getReading();
-        </script>
+        setInterval(updateSleepTimer, 1000);
+      </script>
     </body>
   </html>
   )";
@@ -54,11 +69,16 @@ void handleHello() {
 
 void handleReading() {
     float reading = readVoltage();
-    server.send(200, "text/plain", String(reading, 2));  // Two decimal places
+    String json = "{\"voltage\":" + String(reading, 2) + ",\"bootCount\":" + String(bootCount) + "}";
+    server.send(200, "application/json", json);
 }
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+
+  ++bootCount;
+  Serial.println("Boot count: " + String(bootCount));
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.print("Connecting to WiFi ");
@@ -79,9 +99,22 @@ void setup() {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  // added code to wake up
+  esp_sleep_enable_timer_wakeup(SLEEP_DURATION);
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)wakeupPin, LOW);
+
+
+    Serial.println("Setup ESP32 to sleep for " + String(SLEEP_DURATION / 1000000) +
+  " Seconds");
 }
 
 void loop() {
   server.handleClient();
-  delay(2);
+
+  if (millis() > AWAKE_TIME) { // Check if awake time has passed
+    Serial.println("Going to deep sleep now");
+    Serial.flush();
+    esp_deep_sleep_start(); // Enter deep sleep
+  }
 }
